66152cbc070a8fd278db0b0d0994fbc2a265aa10
Alex Abraham <alex.abraham@vimeo.com>
2023-05-24 23:08:20 -0400

    created bot for the verge

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import llms
 import tweepy
 import os
+import feedparser
+from bs4 import BeautifulSoup
+import re
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 
 def send_tweet(tweet):
@@ -19,46 +25,86 @@ def send_tweet(tweet):
 
 
 def generate_gpt_response(model, prompt) -> str:
-    result = model.complete(prompt, temperature=0.8, max_tokens=1000)
+    result = model.complete(prompt, temperature=0.4, max_tokens=1000)
     return result.text
 
 
-def format_gpt_response(response_text: str):
-    response_text = response_text.strip()
+def get_mongo_engine():
+    uri = "mongodb+srv://{}:{}@{}/?retryWrites=true&w=majority".format(
+        os.getenv("MONGO_USER"),
+        os.getenv("MONGO_PASS"),
+        os.getenv("MONGO_HOST")
+    )
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
 
-    response = {
-        "tweet": None,
-        "hashtags": None
-    }
-    for line in response_text.split("\n"):
-        if line.startswith("Tweet: "):
-            response["tweet"] = line[7:]
-        elif line.startswith("Hashtags: "):
-            response["hashtags"] = line[10:]
+    # Send a ping to confirm a successful connection
+    client.admin.command('ping')
+    print("INFO: successfuly connected to MongoDB")
 
-    return response
+    # connect to the news-bot database
+    return client['news-bot']
 
+
+def article_exists(collection, link):
+    return collection.find_one({"link": link}) is not None
+
+
+def get_verge_articles():
+    url = "https://www.theverge.com/rss/index.xml"
+    feed = feedparser.parse(url)
+
+    articles = []
+    for entry in feed.entries:
+        content_html = entry.content[0].value
+        soup = BeautifulSoup(content_html, 'html.parser')
+        content = re.sub('\s+', ' ', soup.get_text()).strip()
+        articles.append({
+            "headline": entry.title,
+            "date": entry.published,
+            "link": entry.id,
+            "content": content
+        })
+
+    return articles
 
 def get_prompt_from_news_article(article_text):
     return """
-        Please write a tweet (max 250 characters) summarizing the following article:
+        Please write a tweet summarizing the following article:
         
-        {}
+        "{}"
+        
+        The tweet can have a maximum of 200 characters.
     """.format(article_text)
 
 def run():
+    # initialize mongodb
+    db = get_mongo_engine()
+    db_articles = db['articles']
+
+    # get articles from verge
+    articles = get_verge_articles()
+
+    # generate tweets for each article with ChatGPT that we haven't seen before
     model = llms.init(model='gpt-3.5-turbo')
+    for article in articles:
+        if article_exists(db_articles, article["link"]):
+            continue
 
-    test = """
-    Image: Remedy Entertainment Alan Wake 2 just got an official release date — October 17th — but you won’t be able to buy a physical copy of the game, developer Remedy Entertainment revealed in an FAQ on Wednesday. Remedy has three arguments as to why: it says that many players have shifted to only buying games digitally, not releasing the game on disc keeps the price down, and the studio didn’t want to require a separate download even if it released a disc product. Here’s the company’s full explanation, from the FAQ: Why is Alan Wake 2 a digital-only release? There are many reasons for this. For one, a large number of have shifted to digital only. You can buy a Sony PlayStation 5 without a disc drive and Microsoft’s Xbox Series S is a digital only console. It is... Continue reading…
-    """
-    prompt = get_prompt_from_news_article(test)
+        # generate tweet
+        print("Generating Tweet for article: {}".format(article["headline"]))
+        prompt = get_prompt_from_news_article(article["content"])
+        article["tweet"] = generate_gpt_response(model, prompt)
+        article["tweet"] += "\n{}".format(article["link"])
+        print(article["tweet"])
 
-    response = generate_gpt_response(model, prompt)
-    print(response)
+        # send tweet
+        print("Sending Tweet for article: {}".format(article["headline"]))
+        twitter_status = send_tweet(article["tweet"])
+        print(twitter_status)
 
-    # twitter_status = send_tweet(response["tweet"])
-    # print(twitter_status)
+        # store record in database
+        db_articles.insert_one(article)
 
 
 if __name__ == "__main__":
